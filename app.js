15f76a117095c2a12de2b99a382fbb16e9be09b8
wali <waliizafri@gmail.com>
2024-06-07 14:56:59 +0500

    Removed database and uploaded images

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-const session = require('express-session');
-const projects = require("./models/project");
 const path = require('path');
 const app = express();
 
@@ -10,58 +8,16 @@ require('dotenv').config();
 
 const gmailAddress = process.env.GMAIL_ADDRESS;
 const appPassword = process.env.APP_PASSWORD;
-const gmailPassword = process.env.GMAIL_PASSWORD;
 
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-  secret: 'your_secret_key',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } // Set to true in production with HTTPS
-}));
-
-function isAuthenticated(req, res, next) {
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect('/adminLogin');
-  }
-}
-
 app.get('/', async (req, res) => {
-  const project = await projects.find();
-  res.render('index', { project });
+  res.render('index');
 });
 
-app.get('/adminLogin', (req, res) => {
-  const user = { email: '', password: '' };
-  res.render('adminLogin', { user });
-});
-
-app.post('/adminLogin', (req, res) => {
-  const { email, password } = req.body;
-  if (email === gmailAddress && password === gmailPassword) {
-    req.session.user = { email };
-    res.redirect('/adminPanel');
-  } else {
-    res.render('adminLogin', { user: { email, password: '' }, error: 'Invalid email or password' });
-  }
-});
-
-app.get('/adminPanel', isAuthenticated, (req, res) => {
-  res.render('adminPanel');
-});
-
-app.post('/', isAuthenticated, async (req, res) => {
-  const { name, description, image, webLink } = req.body;
-  const newProject = new projects({ name, description, image, webLink });
-  await newProject.save();
-  res.redirect('/');
-});
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -96,9 +52,6 @@ app.post('/process_form', (req, res) => {
   });
 });
 
-// Do not start the server here when running on Netlify
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3001');
-// });
-
-module.exports = app;  // Export the app for serverless usage
+app.listen(3002, () => {
+  console.log('Server is running on port 3002');
+});
--- functions/server.js
+++
@@ -1,4 +0,0 @@
-const serverless = require('serverless-http');
-const app = require('../app');  // Import the app from the root directory
-
-module.exports.handler = serverless(app);
