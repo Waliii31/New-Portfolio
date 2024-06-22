14869a245fe3202ce814395762ce8d55900701ea
wali <waliizafri@gmail.com>
2024-06-23 01:38:07 +0500

    Update repository with new changes

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Script loaded successfully")
+
+// Form submission code
+// $(document).ready(function() {
+//     $('#contactForm').submit(function(event) {
+//         event.preventDefault(); // Prevent the default form submission
+
+//         $.ajax({
+//             type: 'POST',
+//             url: 'submit_form.php',
+//             data: $(this).serialize(),
+//             success: function(response) {
+//                 console.log('Form Submitted Successfully:', response);
+//                 // Display a success message or redirect the user
+//             },
+//             error: function(error) {
+//                 console.error('Error:', error);
+//                 // Display an error message
+//             }
+//         });
+//     });
+// });
+
+
+// Animation code 
+
